5c8e6976fe4527aa7e3e6f6222a4c87b246fbdca
yohon617 <yohon617@yahoo.com>
2021-05-26 17:49:38 -0700

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -129,6 +129,10 @@ const controlAddRecipe = async function (newRecipe) {
     }
 };
 
+const newFeature = function () {
+    console.log('welcome to the app');
+};
+
 const init = function () {
     bookmarksView.addHandlerRender(controlBookmarks);
     recipeView.addHandlerRender(controlRecipes);
@@ -137,7 +141,7 @@ const init = function () {
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
     addRecipeView.addHanlderUpload(controlAddRecipe);
-    console.log('Welcome');
+    newFeature();
 };
 
 init();
